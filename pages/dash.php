2a3c16e9bfa8061a257a6f6c5d3e3dac5ff699f8
jbscosoft <johnkalungi1@gmail.com>
2018-11-01 17:00:19 +0300

    eve

--- pages/dash.php
+++ pages/dash.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" href="../dist/img/ico.png" type="">
   <title>
   <?php 
 session_start(); 
